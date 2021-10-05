a9cc96305389a0bb6a9cfe1ba98e6f0bf6388562
iritu <iritstempler@gmail.com>
2021-10-05 12:33:23 +0300

    initial images and json

---
+++ src/API/items.js
@@ -0,0 +1,55 @@
+import bird from '../Img/bird-with-berries.jpg'
+import dolphin from '../Img/dolphin.jpg'
+import fox from '../Img/iceland-arctic-fox.jpg'
+import penguins from '../Img/penguins.jpg'
+import rabbit from '../Img/rabbit.jpg'
+import redFox from '../Img/red-fox.jpg'
+import tiger from '../Img/tiger.jpg'
+import wolf from '../Img/wolf.jpg'
+
+const data = [
+
+  {
+    "char_id": 1,
+    "name": "Bird with berries",
+    "img": `${bird}`
+  },
+  {
+    "char_id": 2,
+    "name": "Under water dolphin",
+    "img": `${dolphin}`
+  },
+  {
+    "char_id": 3,
+    "name": "Iceland arctic fox",
+    "img": `${fox}`
+  },
+  {
+    "char_id": 4,
+    "name": "penguins in the snow",
+    "img": `${penguins}`
+  },
+  {
+    "char_id": 5,
+    "name": "Bunny rabbit in nature",
+    "img": `${rabbit}`
+  },
+  {
+    "char_id": 6,
+    "name": "Red fox",
+    "img": `${redFox}`
+  },
+  {
+    "char_id": 7,
+    "name": "Wild tiger",
+    "img": `${tiger}`
+  },
+  {
+    "char_id": 7,
+    "name": "Grey wolf",
+    "img": `${wolf}`
+  }
+
+]
+
+export default data;
